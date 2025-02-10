545db8e372c52fd51540ddf9e85137cf6d607685
anandasatriaa <anandasatriaf6@gmail.com>
2025-02-10 12:59:07 +0700

    fix: plugins js

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -197,7 +197,10 @@
     <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
     <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
     <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
-    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/plugins.js') }}"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <script src="{{ asset('assets/libs/choices.js/public/assets/scripts/choices.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/flatpickr/flatpickr.min.js') }}"></script>
 
     <!-- particles js -->
     <script src="{{ asset('assets/libs/particles.js/particles.js') }}"></script>
